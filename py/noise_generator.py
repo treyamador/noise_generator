38d7a4a12abbee6deb1472862d1ac5911c8c6cb0
TreyAmador <tmamador@cpp.edu>
2017-08-21 23:00:42 -0700

    revise WAV

--- py/noise_generator.py
+++ py/noise_generator.py
@@ -2,49 +2,57 @@ import wave
 import math
 
 
-
 class WAV:
 
-    def __init__(self,filename,nchannels=2,sampwidth=2,framerate=44100,nframes=441000):
-        ''' init new wav file '''
-        self.wav = self._init_wav(filename)
-        self.set_attr(nchannels,sampwidth,framerate,nframes)
+    def __init__(self,nchannels=2,sampwidth=2,framerate=44100,nframes=441000):
+        ''' args: path, nchannels, sampwidth, framerate, nframes, data '''
+        self.set_nchannels(nchannels)
+        self.set_sampwidth(sampwidth)
+        self.set_framerate(framerate)
+        self.set_nframes(nframes)
 
-    def set_attr(self,nchannels,sampwidth,framerate,nframes):
+    def set_attr(self,wav,nchannels,sampwidth,framerate,nframes):
+        wav.setnchannels(nchannels)
+        wav.setsampwidth(sampwidth)
+        wav.setframerate(framerate)
+        wav.setnframes(nframes)
+        return wav
+
+    def set_nchannels(self,nchannels):
         self.nchannels = nchannels
+
+    def set_sampwidth(self,sampwidth):
         self.sampwidth = sampwidth
+
+    def set_framerate(self,framerate):
         self.framerate = framerate
+        
+    def set_nframes(self,nframes):
         self.nframes = nframes
-        self.wav.setnchannels(nchannels)
-        self.wav.setsampwidth(sampwidth)
-        self.wav.setframerate(framerate)
-        self.wav.setnframes(nframes)
 
     def set_data(self,data):
-        if isinstance(data,list):
-            self.wav.writeframesraw(bytes(data))
+        self.data = data
 
-    def _init_wav(self,filename):
-        return wave.open(filename,'wb')
+    def write_file(self,path):
+        with wave.open(path,'wb') as wav:
+            wav = self.set_attr(wav,
+                self.nchannels,self.sampwidth,
+                self.framerate,self.nframes)
+            wav.writeframesraw(bytes(self.data))
 
-    def __del__(self):
-        self.wav.close()
+    def read_file(self,path):
+        pass
 
 
 def create_wave(filename):
-    wav = WAV(filename)
+    wav = WAV()
     frames = [int(math.sin(x)) for x in range(44100*10)]
     wav.set_data(frames)
-
-
+    wav.write_file(filename)
 
 def driver():
     create_wave('sample.wav')
     
-
-
-
-
 if __name__ == '__main__':
     driver()
 
