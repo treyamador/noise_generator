73abd49130a3f3f56ad0ed408a5524cbbd672e22
TreyAmador <tmamador@cpp.edu>
2017-08-22 06:43:34 -0700

    working a maj tone

--- py/noise_generator.py
+++ py/noise_generator.py
@@ -5,7 +5,7 @@ import math
 
 class WAV:
 
-    def __init__(self,nchannels=2,sampwidth=2,framerate=44100,nframes=4410000):
+    def __init__(self,nchannels=1,sampwidth=1,framerate=44100,nframes=4410000):
         ''' args: path, nchannels, sampwidth, framerate, nframes, data '''
         self.set_nchannels(nchannels)
         self.set_sampwidth(sampwidth)
@@ -45,20 +45,25 @@ class WAV:
         pass
 
 
-def create_wave(filename):
+def create_single_tone_wave(filename):
+    # WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))
     wav = WAV()
-    frames = [int(math.sin(x)*128)+127 for x in range(44100*100)]
-
-    #for frame in frames[:100]:
-    #    print(frame,end=' ')
-    #print('\n')
-
+    frames = [int(math.sin(x/100)*128)+127 for x in range(44100*100)]
     wav.set_data(frames)
     wav.write_file(filename)
 
+def create_a_note_wav(filename):
+    sinusoid = [int(math.sin(880*math.pi*x/44100)*128+128) for x in range(4410000)]
+    wav = WAV()
+    wav.set_data(sinusoid)
+    wav.write_file(filename)
+
+
 def driver():
-    create_wave('sample.wav')
+    create_a_note_wav('sample.wav')
     
+
 if __name__ == '__main__':
     driver()
+
 
